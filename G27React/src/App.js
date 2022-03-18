96c3a75608370054d082475ace54be6dbf071dfc
Samy Touabi <stoua026@uottawa.ca>
2022-03-18 19:13:19 -0400

    added files
    the Modules are missing. to add them, go on the terminal and write:
    
    npm install

---
+++ G27React/src/App.js
@@ -0,0 +1,48 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/create">
+              <Create />
+            </Route>
+
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+
+            <Route path="*">
+              <NotFound />
+            </Route>
+
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+// const title = 'Welcome to the new blog';
+// const likes = 50;
+// const link = "http://www.google.com";
+// <h1>{title}</h1>
+// <p>Liked {likes} times</p>
+// <p>Random number: {Math.random() * 10} </p>
+// <a href={link} target="_blank">Google Site</a>
