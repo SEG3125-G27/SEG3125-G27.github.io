3130d407e0e6e6888fa6116d8b88be16d3fe34ce
Kishan <kmrug025@uottawa.ca>
2022-03-20 15:59:37 -0400

    add footer file

---
+++ G27React/src/footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+
+            <p style={{
+                textAlign: 'center',
+                padding: '15px',
+                color: "silver",
+                fontFamily: 'Times New Roman',
+                fontSize: 'large'
+            }} >
+                React Website Made by G27
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
